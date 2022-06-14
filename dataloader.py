1c1587acbabf0141580ae72a68bcbf5df8ed2581
Alejandro Ciuba <alejandrociuba@gmail.com>
2022-06-14 15:17:44 -0400

    Made data in dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -9,12 +9,19 @@ from transformers import Wav2Vec2Processor, HubertModel
 import torch as th
 import time
 
+import pandas as pd
+
 class TweetLoader(Dataset):
     def __init__(self, path, transformer):
        # CBeaune.txt  CZacharopoulou.txt  dataloader.py  franckriester.txt  JLMelenchon.txt  MinColonna.txt 
-       self.data = pd.read_csv(path, sep='\t')
-       if transformer == 'TODO':
-        self.tokenizer = TODO
+       dataset = pd.read_pickle('train_parser/dataset_df.pkl')
+       dataset = dataset[['tweet', 'label']] #Remove author names
+
+       self.data = dataset.to_dict('record')
+
+        if transformer == 'TODO':
+            self.tokenizer = TODO
+
 
     def __len__(self):
         return len(self.data)
--- train_parser/data.py
+++ train_parser/data.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 # Returns data, label tuples
 def load_data(dataset:pd.DataFrame, quantity:int=1):
-    return dataset.sample(quantity).tolist()
+    pass
